a084a6a15446a870720310e9ace3ecbedb90835e
mtuit <micktuit@gmail.com>
2016-12-12 15:48:22 +0100

    Proef handler

---
+++ handlers/proef_handler.php
@@ -0,0 +1,22 @@
+<?php 
+	include '../includes/database_functions.php';
+
+	db_open();
+	if (isset($_GET['value'])) {
+
+		if($_GET['value'] == 'opslaan') {
+			
+			$PROEF_NAAM = $_POST['researchName'];
+			$sql = "INSERT INTO PROEF ([PROEF_NAAM]) VALUES ('$PROEF_NAAM')";
+			$stmt = db_query($sql);
+
+		} else if ($_GET['value'] == 'verwijderen') {
+			echo 'Verwijderen!';
+		} else {
+			echo 'Get is niet opslaan of verwijderen';
+		}
+
+	} else {
+		echo 'GET not set!'; 
+	}
+?>
