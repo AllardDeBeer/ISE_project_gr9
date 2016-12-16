fe9204e6054697b96e126aa9e802ca1fa14cd6b0
jayzz911 <jayzz911@hotmail.com>
2016-12-16 03:46:31 +0100

    wachtwoord_vergeten

---
+++ includes/FunctionsJ.php
@@ -0,0 +1,63 @@
+<?php
+include 'includes/database_functions.php';
+db_open();
+$serverName = "(local)\SQLEXPRESS";
+$connectionInfo = array( "Database"=>"DonkeyKong",  "UID"=>"sa", "PWD"=>"rammus");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+function checkVeldenIngevuld()
+{
+    if(empty($_POST['user']))
+	{
+        die("geen gebruikersnaam ingevuld");
+		return false;
+    }
+	if(empty($_POST['Password']))
+	{	
+		die("vul uw wachtwoord in");
+		return false;
+	}
+	if($_POST['Password'] != $_POST['PasswordC'])
+	{
+			echo 'pass:';
+			echo $_POST['Password'] ;
+			echo 'confirm:';
+			echo $_POST['PasswordC'] ;
+		die("Wachtwoorden komen niet overeen");
+
+			return false;
+	
+	}
+	else {
+		echo'velden ingevuld';
+		return true;
+	}
+		
+}
+
+Function Wachtwoordvergeten($user,$pass,$passC,$answer,$sql){	
+$serverName = "(local)\SQLEXPRESS";
+$connectionInfo = array( "Database"=>"DonkeyKong",  "UID"=>"sa", "PWD"=>"rammus");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+$update = "UPDATE gebruiker SET wachtwoord = '$pass' WHERE gebruikersnaam = '$user'";
+
+		if(checkVeldenIngevuld() == false){
+			 
+		}
+		else {
+			$stmt = sqlsrv_query($conn,$sql);
+			if( $stmt === false) {
+    die( print_r( sqlsrv_errors(), true) );
+}
+			$result = sqlsrv_fetch_array ($stmt, SQLSRV_FETCH_NUMERIC);
+			if(empty($result[0]))
+				echo 'Antwoord op de vraag is incorrect';
+			else if(!empty($result[0])){
+				sqlsrv_query($conn,$update);
+			}		
+		}
+			
+			
+}
+
+?>
--- wachtwoord_vergeten.php
+++ wachtwoord_vergeten.php
@@ -12,15 +12,14 @@
   </head>
   <body>
   <?php
-  
-  include 'includes/FunctionsJ.php';
-  include 'includes/database_functions.php';
+  $gebruiker = $_GET[user];
+  include '/includes/database_functions.php';
   db_open();
   $result = db_query("SELECT vraag
 FROM GEBRUIKER G INNER JOIN BEVEILIGINGSVRAGEN BV ON  G.VRAAG_ID = BV.VRAAG_ID
-WHERE GEBRUIKERSNAAM = " . $_get[user] ."");
-	$vraag = db_fetchAssoc($result);
-	
+WHERE GEBRUIKERSNAAM = '$gebruiker' ");
+	$vraag = db_fetchNumeric($result);
+	echo'
     <div class="expanded row">
       <div class="column large-3 left-menu">
         <div class="expanded row logo">
@@ -32,22 +31,26 @@ WHERE GEBRUIKERSNAAM = " . $_get[user] ."");
           </div>
         </div>
 	</div>
-
       <div class="column large-9 right-screen">
         <div class="large-12">
           <div class="container">
               <div class ="row">
 				<h2 class="textcenter">Wachtwoord vergeten</h2>
-			  	<form action="wachtwoordvergetenHandler.html">
+			  	<form action="wachtwoord_vergetenHandler.php" method="post"">
 				<div class="row">
 
 					<div class="large-7 large-offset-2">
-						echo "$vraag"
-						<h4 class="">  ;</h4>
+					
+						
+							
+						<h4 class="">';
+						print($vraag[0]);
+						echo' </h4>
 						
 						<input type="text" name="Answer"><br>
 						Nieuw wachtwoord:<input type="text" name="Password"><br>
-						Herhaal nieuw wachtwoord<input type="text" name="PasswordConfirm"><br>
+						Herhaal nieuw wachtwoord<input type="text" name="PasswordC"><br>
+						<input type ="hidden" name="user" Value="'; echo htmlspecialchars($gebruiker); echo '">
 						<input type="submit" class="button" value="Submit">
 					</div>
 			  </form>
@@ -55,10 +58,7 @@ WHERE GEBRUIKERSNAAM = " . $_get[user] ."");
         </div>
       </div>
     </div>
+	';
 ?>
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/what-input.js"></script>
-    <script src="js/vendor/foundation.js"></script>
-    <script src="js/app.js"></script>
   </body>
 </html>
