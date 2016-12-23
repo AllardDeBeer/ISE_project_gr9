28db81195ed7f6c3d3a214c27d0263184e30ea09
mtuit <micktuit@gmail.com>
2016-12-23 13:49:05 +0100

    Laatste login toegevoegd

--- handlers/login_handler.php
+++ handlers/login_handler.php
@@ -8,13 +8,17 @@
           $username = $_POST['username'];
           $password = $_POST['password'];
 
-          $stmt = db_query("SELECT VOORNAAM, ACHTERNAAM, GEBRUIKERSNAAM, WACHTWOORD FROM GEBRUIKER WHERE GEBRUIKERSNAAM = '$username'");
+          $stmt = db_query("SELECT VOORNAAM, TUSSENVOEGSEL, ACHTERNAAM, GEBRUIKERSNAAM, WACHTWOORD, DATUMLAATSTELOGIN FROM GEBRUIKER WHERE GEBRUIKERSNAAM = '$username'");
           $result = db_fetchAssoc($stmt);
 
           if($password == $result['WACHTWOORD']) {
                $_SESSION['username'] = $result['GEBRUIKERSNAAM'];
                $_SESSION['firstname'] = $result['VOORNAAM'];
+               $_SESSION['insertion'] = $result['TUSSENVOEGSEL'];
                $_SESSION['surname'] = $result['ACHTERNAAM'];
+               $_SESSION['last_login'] = date_format($result['DATUMLAATSTELOGIN'], 'd/m/Y H:i:s');
+
+               $stmt = db_query("UPDATE GEBRUIKER SET DATUMLAATSTELOGIN = GETDATE() WHERE GEBRUIKERSNAAM = ".$result['GEBRUIKERSNAAM']);
                header('Location: ../index.php');
           } else {
                header("Refresh:0; ../login.html");
