c502f6e2f5b9ef9894ea715811229209632ce0fb
Dion Rats <dionrats@gmail.com>
2017-01-10 12:40:16 +0100

    chart.js moved from cdn to local

--- index.php
+++ index.php
@@ -241,7 +241,7 @@
     <script src="js/vendor/jquery.js"></script>
     <script src="js/vendor/what-input.js"></script>
     <script src="js/vendor/foundation.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.js"></script>
+    <script src="js/vendor/chart.js"></script>
     <script src="js/vendor/jquery-ui.min.js"></script>
     <script src="js/app.js"></script>
   </body>
