175ec271ce7c573b868977a331c364063715b636
AllardDeBeer <allarddebeer@gmail.com>
2017-01-19 15:45:13 +0100

    aap aan onderzoek toevoegen
    bij mij werkt het zonder errors

--- containers/aap_In_onderzoek.php
+++ containers/aap_In_onderzoek.php
@@ -14,29 +14,75 @@ db_open();
   <!-- </div> -->
   <hr>
   <!-- <div class="column large-12"> -->
-    <label for="addMonkey">apen toevoegen</label>
-    <input type="text" name="addMonkey" id="searchInput" onkeyup="showResult(this.value, 10, 'livesearch')" autocomplete='off'>
+  <div class="row">
+  <div class="column small-2"><label for="addMonkey">Aap ID</label>
+    <input  type="text" name="addMonkey" id="searchInputId" onkeyup="setValue(),showResult(document.getElementById('searchValues').value+'||'+document.getElementById('insertValues').value, 10, 'livesearch')" autocomplete='off'>
+	</div>	
+	<div class="column small-2"><label for="addMonkey">Behandelgroep</label>
+    <input type="text" name="addMonkey" id="searchInputGroup" onkeyup="setValue(),showResult(document.getElementById('searchValues').value+'||'+document.getElementById('insertValues').value, 10, 'livesearch')" autocomplete='off'>
+	</div>
+	<div class="column small-2"><label for="addMonkey">Diersoort</label>
+    <input type="text" name="addMonkey" id="searchInputAnimal" onkeyup="setValue(),showResult(document.getElementById('searchValues').value+'||'+document.getElementById('insertValues').value, 10, 'livesearch')" autocomplete='off'>
+	</div>	
+	<div class="column small-2"><label for="addMonkey">Geslacht</label>
+    <input type="text" name="addMonkey" id="searchInputGender" onkeyup="setValue(),showResult(document.getElementById('searchValues').value+'||'+document.getElementById('insertValues').value, 10, 'livesearch')" autocomplete='off'>
+	</div>	
+	<div class="column small-2"><label for="addMonkey">Gewicht</label>
+    <input type="text" name="addMonkey" id="searchInputWeight" onkeyup="setValue(),showResult(document.getElementById('searchValues').value+'||'+document.getElementById('insertValues').value, 10, 'livesearch')" autocomplete='off'>
+	</div>
+	<div class="column small-2"><label for="addMonkey">Dominant</label>
+    <input type="text" name="addMonkey" id="searchInputDom" onkeyup="setValue(),showResult(document.getElementById('searchValues').value+'||'+document.getElementById('insertValues').value, 10, 'livesearch')" autocomplete='off'>
+	</div>
+
+</div>
+    <input id= "searchValues" type="hidden" value="">
+	<input id= "insertValues" type="hidden" value="">
+	
+<?php //+document.getElementById('insertValues')?>
+
+
+
+<script>
+function setValue() {
+	var ID = document.getElementById("searchInputId").value;
+	var GROUP = document.getElementById("searchInputGroup").value;
+	var ANIMAL = document.getElementById("searchInputAnimal").value;
+	var GENDER = document.getElementById("searchInputGender").value;
+	var WEIGHT = document.getElementById("searchInputWeight").value;
+	var DOM = document.getElementById("searchInputDom").value;
+    document.getElementById("searchValues").value = ID+"‽"+GROUP+"‽"+ANIMAL+"‽"+GENDER+"‽"+WEIGHT+"‽"+DOM;
+}
+function changeBox(ID) {
+	
+	document.getElementById("insertValues").value += ID+"‽";
+}
+</script>
+
+
+	
+	
     <table>
       <thead>
           <tr>
             <th>Aap id</th>
-            <th>geboorte datum</th>
+            <th>Behandelgroep</th>
             <th>Diersoort</th>
-            <th>geslacht</th>
-			<th>gewicht</th>
-			<th>geselecteerd</th>
+            <th>Geslacht</th>
+			<th>Gewicht</th>
+			<th>Dominant</th>
+			<th>Geselecteerd</th>
           </tr>
         </thead>
 		
 		
-        <tbody id="livesearch">
+        <tbody  id="livesearch">
          <!-- Live reloaded content -->
       </tbody>
 	  
-	  <input type="button" value="opslaan" class="button right" onclick="showResult('nietNuttig', 11, 'livesearch')">
+	  
     </table>
+	<input type="button" value="opslaan" class="button right" onclick="showResult(document.getElementById('insertValues').value, 11, 'livesearch')">
 	
-		
     </form>
 </div>
 </div>
--- handlers/aapInOnderzoek_handler.php
+++ handlers/aapInOnderzoek_handler.php
@@ -1,18 +1,40 @@
 <?php
 session_start();
 include '../includes/database_functions.php';
-$monkeys =$_GET["q"];
-$monkeys = ltrim($monkeys, "@");
-$monkeys = ltrim($monkeys, "[");
-$monkeys = rtrim($monkeys, "]");
-$array= array();
-$array= explode( '][', $monkeys );
-$pinned="";
+$monkeyIDs =$_GET["q"];
+$monkeyIDs=rtrim($monkeyIDs, "‽");
+
+
+$allIdArray= array();
+$allIdArray= explode( '‽', $monkeyIDs );
+$idArray= array();
+$count=array_count_values($allIdArray);
+$insertIdArray= array();
+
+for ($x = 0; $x < count($allIdArray); $x++) {
+if($count[$allIdArray[$x]]%2==1){
+	$idArray[]=$allIdArray[$x];
+}	
+}
+for ($x = 0; $x < count($idArray); $x++) {
+	$IDcount=array_count_values($idArray);
+	if($IDcount[$idArray[$x]]>1){
+	$idArray[$x]='';
+}
+else{
+$insertIdArray[]=$idArray[$x];
+}
+}
+
 $currentResearch=$_SESSION['onderzoek'];
 
 db_open();
 
-foreach ($array as $monkey) {
+
+
+
+
+foreach ($insertIdArray as $monkey) {
 		
 	db_query("insert into aapInOnderzoek(aap_id, onderzoek_id) values(". $monkey . ",'$currentResearch ') ");
 	
