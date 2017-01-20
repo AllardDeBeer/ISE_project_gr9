c7bb7a5457688a6961256cf55c48be1a0fbc6153
mtuit <micktuit@gmail.com>
2017-01-20 13:44:52 +0100

    urls universeel gemaakt

--- js/app.js
+++ js/app.js
@@ -117,21 +117,21 @@ function newTest(researchName, value){
       // Check if proef isValid
       if (this.responseText == 'notValid') {
         // Check if not already on page
-        if (window.location.href == ("http://localhost/index.php?m=13#nieuw_proef" || "http://localhost:8080/index.php?m=13#beheer_proef")) {
+        if (window.location.href == ("../index.php?m=13#nieuw_proef" || "../index.php?m=13#beheer_proef")) {
           window.location.reload();
         } else {
           window.location.replace("../index.php?m=13#nieuw_proef");
         }
       } else if (this.responseText == 'isValid') {
         // Check if not already on page
-        if (window.location.href == ("http://localhost/index.php?m=16#nieuw_proef" || "http://localhost:8080/index.php?m=16#beheer_proef")) {
+        if (window.location.href == ("../index.php?m=16#nieuw_proef" || "../index.php?m=16#beheer_proef")) {
           window.location.reload();
         } else {
           window.location.replace("../index.php?m=16#nieuw_proef");
         }
       } else if (this.responseText == 'noResearchName') {
         console.log('test'); 
-        if (window.location.href == ("http://localhost/index.php?m=17#nieuw_proef" || "http://localhost:8080/index.php?m=17#beheer_proef")) {
+        if (window.location.href == ("../index.php?m=17#nieuw_proef" || "../index.php?m=17#beheer_proef")) {
           window.location.reload();
         } else {
           window.location.replace("../index.php?m=17#nieuw_proef");
@@ -149,13 +149,13 @@ function manageTest(value, researchName) {
     if (this.readyState==4 && this.status==200) { 
       console.log(this.responseText);
       if (this.responseText == 'waarden') {
-        if (window.location.href == ("http://localhost/index.php?m=14#beheer_proef" || "http://localhost:8080/index.php?m=14#beheer_proef")) {
+        if (window.location.href == ("../index.php?m=14#beheer_proef" || "../index.php?m=14#beheer_proef")) {
           window.location.reload();
         } else {
           window.location.replace("../index.php?m=14#beheer_proef")
         }
       } else if (this.responseText == 'gelukt') {
-        if (window.location.href == ("http://localhost/index.php?m=15#beheer_proef" || "http://localhost:8080/index.php?m=15#beheer_proef")) {
+        if (window.location.href == ("../index.php?m=15#beheer_proef" || "../index.php?m=15#beheer_proef")) {
           window.location.reload();
         } else {
           window.location.replace("../index.php?m=15#beheer_proef")
@@ -173,13 +173,13 @@ function updateMonkeys(str) {
     if (this.readyState == 4 && this.status == 200) {
       console.log(this.responseText);
       if (this.responseText == 'gelukt') {
-        if (window.location.href == ("http://localhost/index.php?m=19#beheer_apen" || "http://localhost:8080/index.php?m=19#beheer_apen")) {
+        if (window.location.href == ("../index.php?m=19#beheer_apen" || "../index.php?m=19#beheer_apen")) {
           window.location.reload();
         } else {
           window.location.replace("../index.php?m=19#beheer_apen")
         }
       } else if (this.responseText == 'mislukt') {
-        if (window.location.href == ("http://localhost/index.php?m=20#beheer_apen" || "http://localhost:8080/index.php?m=20#beheer_apen")) {
+        if (window.location.href == ("../index.php?m=20#beheer_apen" || "../index.php?m=20#beheer_apen")) {
           window.location.reload();
         } else {
           window.location.replace("../index.php?m=20#beheer_apen")
@@ -196,7 +196,7 @@ function addExistingTest(testName, status, id) {
   xmlhttp.onreadystatechange=function() {
     if (this.readyState == 4 && this.status == 200) {
       if (this.responseText == 'gelukt') {
-        if (window.location.href == ("http://localhost/index.php?m=18#bestaande_proef" || "http://localhost:8080/index.php?m=18#bestaande_proef")) {
+        if (window.location.href == ("../index.php?m=18#bestaande_proef" || "../index.php?m=18#bestaande_proef")) {
           window.location.reload();
         } else {
           window.location.replace("../index.php?m=18#bestaande_proef");
